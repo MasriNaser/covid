65b35088dab24ab6104c54d0c284737056fd95a9
MasriNaser <Naser.Masri@cronos.be>
2020-06-04 10:32:30 +0200

    Updates

---
+++ precache-manifest.616de67aeb2c21a558bdd8962b5341e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b77f774b927b2bc111b51727f4b12f0",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "2e60611df6cd7a506cd8",
+    "url": "/covid/static/css/main.8c1f62f7.chunk.css"
+  },
+  {
+    "revision": "a0264786d99ec032ff4b",
+    "url": "/covid/static/js/2.bb034e48.chunk.js"
+  },
+  {
+    "revision": "2c24acf63e18487b21e343b8335b2718",
+    "url": "/covid/static/js/2.bb034e48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e60611df6cd7a506cd8",
+    "url": "/covid/static/js/main.7caea4bf.chunk.js"
+  },
+  {
+    "revision": "71562707c8d5396854e9",
+    "url": "/covid/static/js/runtime-main.bc4d70b9.js"
+  },
+  {
+    "revision": "f044eb30016560ea09bebccf6750dc6c",
+    "url": "/covid/static/media/covid19.f044eb30.jpg"
+  }
+]);
